ef7eca341862c3c202e0683471e43bdaf979c90d
Chanuth <it20074418@my.sliit.lk>
2023-11-26 09:29:12 +0530

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,14 @@
-import { Component, ViewChild, AfterViewInit} from '@angular/core';
-import { PostComponent } from './post/post.component';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements AfterViewInit{
-  title = 'ang-project';
-  parentMessage:string = 'Message coming from parent component';
-  message?:string;
-  fromChildOutput:string;
-
-  @ViewChild(PostComponent) childComp;
-
-  constructor(){
-    console.log(this.childComp);
-  }
-  ngAfterViewInit(){
-    console.log(this.childComp);
-    this.message = this.childComp.childMessage
-  }
-
-  reciveMessage($event: any) {
-    this.fromChildOutput = $event;
-  }
+export class AppComponent {
+  title = 'project-pro';
 }
